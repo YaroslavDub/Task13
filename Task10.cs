3273a0585594e25428d58c5abb79aa02a0ce90a5
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-13 19:32:13 +0300

    Task10

---
+++ Task10.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine ("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>0) {
+while (a > 999) {
+a = a / 10;
+}
+if (a > 99 && a < 1000) {
+a = a % 10;
+Console.WriteLine(a);
+}
+else {
+Console.WriteLine("3 Числа нет");
+}
+}
+else {
+    while (a < -1000) {
+        a = a / 10;
+    }
+    if (a > -1000 && a < -99){
+        a = a % -10;
+        Console.WriteLine(a);
+    }
+    else {
+        Console.WriteLine("3 Числа нет");
+    }
+}
